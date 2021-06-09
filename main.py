a39f3832ae42c748eb66fed007eb7f3d5a293f9d
Daniel Rogalla <daniel@daniels-imac.speedport.ip>
2021-06-09 13:04:46 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+import simpy
+
+
+
+
+def car(env,id ,  driver):
+    request = driver.request()
+    yield request
+    print('Start parking at %d ' % env.now + str(id))
+    parking_duration = 5
+    yield env.timeout(parking_duration)
+    print('Start driving at %d ' % env.now + str(id))
+    trip_duration = 2
+    yield env.timeout(trip_duration)
+    yield env.timeout(1)
+    driver.release(request)
+
+
+
+if __name__ == '__main__':
+    env = simpy.Environment()
+    driver = simpy.Resource(env, capacity=2)
+    env.process(car(env, 1, driver))
+    env.process(car(env, 2, driver))
+    env.process(car(env, 3, driver))
+
+
+    env.run(until=15)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
