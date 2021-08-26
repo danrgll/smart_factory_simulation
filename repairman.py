b4f7b975dfdd7efea49e7289c7713a667b2e638d
daniel <daniel_rogalla@web.de>
2021-08-26 16:30:54 +0200

    upload

---
+++ repairman.py
@@ -0,0 +1,26 @@
+import random
+import simpy
+from base_elements import Event
+import numpy as np
+
+class Repairman(object):
+    def __init__(self, env: simpy.Environment, location, repair_time: tuple):
+        self.env = env
+        self.busy = False
+        self.location = location
+        self.repair_time_m_s = repair_time
+
+    def work(self, new_job_location, release_resource: Event):
+        # time to arrive broken machine
+        time_to_job_location = np.linalg.norm(self.location - new_job_location)
+        yield self.env.timeout(time_to_job_location)
+        self.location = new_job_location
+        yield self.env.timeout(self.repair_time())
+        self.busy = False
+        release_resource.trigger(False)
+
+    def repair_time(self):
+        """return the processing_time"""
+        return abs(random.normalvariate(self.repair_time_m_s[0], self.repair_time_m_s[1]))
+
+
